ca001a0bcee5d196595f49bd358243439411ecc3
Sam Gardiner <sam.gardiner@orbis.com>
2022-11-21 14:19:18 +0000

    filters and so on working..

--- sample/app.R
+++ sample/app.R
@@ -19,35 +19,56 @@ ui <- fluidPage(
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-          checkboxGroupInput(inputId = "metric",
+        sidebarPanel( 
+          checkboxInput(inputId = "smooth", label = "Smooth data over time?", value = FALSE),
+          checkboxGroupInput(inputId = "metrics",
                        label = "Metric to plot",
-                       choices = neat_metrics
+                       choices = neat_metrics, 
+                       selected = neat_metrics
                        ),
-          checkboxGroupInput(inputId = "stations",
-                             label = "Stations to plot",
-                             choices = raw %>% pull(area_name) %>% unique()
-          )
+          checkboxGroupInput(inputId = "boroughs",
+                             label = "Boroughs to plot",
+                             choices = raw %>% pull(area_name) %>% unique(), 
+                             selected = raw %>% pull(area_name) %>% unique(),
+          ), width = 1
         ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-          "hello", 
+        mainPanel(  width = 9,
            plotOutput("timePlot", height = 1200)
         )
     )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
+server <- function(input, output, session) {
+    
     output$timePlot <- renderPlot({
       
+      
+      
+      filtered_data <- tidy %>% 
+        filter(area_name %in% input$boroughs) %>% 
+        filter(metric_name %in% input$metrics)
+      
+      if(input$smooth) {
+        filtered_data <- filtered_data %>% 
+          group_by(area_name, metric_name) %>% 
+          mutate(metric_value = zoo::rollmean(metric_value, k = 10, fill = c(NA, NA, NA)))
+      } else {
+        # no smoothings
+      }
         
-      tidy %>% 
-        ggplot(aes(x = date, y = metric_value, colour = metric_name)) +
-        geom_line() +
-        facet_wrap(~area_name)
+      if(nrow(filtered_data) !=0){
+        filtered_data %>% 
+          ggplot(aes(x = date, y = metric_value, colour = metric_name)) +
+          geom_line() +
+          facet_wrap(~area_name) + 
+          theme(legend.position = "top") 
+      } else {
+        print("no data selected")
+      }
+      
     })
 }
 
