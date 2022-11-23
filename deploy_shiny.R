ac86a54a0fce8ace11974d63f63889ce2e93e64b
Sam Gardiner <sam.gardiner@orbis.com>
2022-11-23 14:45:49 +0000

    include force in shiny deploy

--- deploy_shiny.R
+++ deploy_shiny.R
@@ -1,7 +1,7 @@
 # command to deploy shiny app
 
 
-rsconnect::deployApp('sample', account = "sam-gardiner")
+rsconnect::deployApp('sample', account = "sam-gardiner", forceUpdate = TRUE)
 
 
 rmarkdown::render("001_explore_data.R", output_options = list(warning=FALSE)) 
